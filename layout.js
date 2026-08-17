9c57c355fa161a3757305bcd002176d3139a8e64
dominioniria <iriadominion@gmail.com>
2026-08-17 13:17:31 +0100

    Delete layout.js

--- layout.js
+++
@@ -1,3 +0,0 @@
-import "./globals.css";
-export const metadata={title:"CMDA Attendance",description:"Attendance portal"};
-export default function RootLayout({children}){return <html lang="en"><body>{children}</body></html>}
