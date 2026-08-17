cfeea8e86c189f85e404f51a37f222588defddd2
dominioniria <iriadominion@gmail.com>
2026-08-17 13:13:20 +0100

    Add files via upload

---
+++ layout.js
@@ -0,0 +1,3 @@
+import "./globals.css";
+export const metadata={title:"CMDA Attendance",description:"Attendance portal"};
+export default function RootLayout({children}){return <html lang="en"><body>{children}</body></html>}
